732565f34476c7f89d59ced24d146615005421a9
Lien Font <fontlien@gmail.com>
2024-07-31 23:50:36 -0400

    initial commit

---
+++ src/main/java/dev/lstt/list_tier/ListTierApplication.java
@@ -0,0 +1,13 @@
+package dev.lstt.list_tier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListTierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListTierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lstt/list_tier/ListTierApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lstt.list_tier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListTierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
